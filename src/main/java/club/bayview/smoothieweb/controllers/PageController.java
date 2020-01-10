72432cd1027aba11a8abd5d665d3d01469f6a594
Seshan Ravikumar <seshan10@me.com>
2020-01-10 09:19:11 -0500

    CMS Reactive WebClient

--- src/main/java/club/bayview/smoothieweb/controllers/PageController.java
+++ src/main/java/club/bayview/smoothieweb/controllers/PageController.java
@@ -2,37 +2,39 @@ package club.bayview.smoothieweb.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-
-
-// REST
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
+// Reactive Web Client
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class PageController {
     @RequestMapping("/page/{slug}")
-    public String requestPage(HttpServletRequest request, Model model, @PathVariable("slug") String slug) throws JsonProcessingException {
+    public String requestPage(Model model, @PathVariable("slug") String slug) throws JsonProcessingException {
+
+        WebClient client = WebClient.create("http://localhost:3000");
+
+        WebClient.RequestBodySpec request = client
+                .method(HttpMethod.GET)
+                .uri("/pages/" + slug);
+
+        String pageJSON = request.exchange().block().bodyToMono(String.class).block();
 
         ObjectMapper mapper = new ObjectMapper();
-        RestClient cms = new RestClient();
-
-        String pageJSON = cms.get("/pages/" + slug);
         PageResponse pageResponse = mapper.readValue(pageJSON, PageResponse.class);
+
+        if(pageResponse.pages.length == 0) {
+            return "404";
+        }
+
         Page page = pageResponse.pages[0];
 
-        model.addAttribute("content", page.content);
+        model.addAttribute("page", page);
         return "page";
     }
 
@@ -48,58 +50,10 @@ class Page {
     public int id;
     public String slug;
     public String date;
+    public boolean display_on_nav;
+    public int parent;
     public String nav_title;
     public String title;
     public String meta;
     public String content;
-}
-
-
-class RestClient {
-
-    private String server = "http://localhost:3000";
-    private RestTemplate rest;
-    private HttpHeaders headers;
-    private HttpStatus status;
-
-    public RestClient() {
-        this.rest = new RestTemplate();
-        this.headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Accept", "*/*");
-    }
-
-    public String get(String uri) {
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.GET, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-        return responseEntity.getBody();
-    }
-
-    public String post(String uri, String json) {
-        HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
-        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.POST, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-        return responseEntity.getBody();
-    }
-
-    public void put(String uri, String json) {
-        HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
-        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.PUT, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-    }
-
-    public void delete(String uri) {
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.DELETE, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
 }
