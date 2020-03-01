9c7408a5ea157c190520c52f43d6c175d899100c
EspiDev <espidev@gmail.com>
2020-03-01 15:42:13 -0500

    Websocket security

--- src/main/java/club/bayview/smoothieweb/controllers/LiveSubmissionController.java
+++ src/main/java/club/bayview/smoothieweb/controllers/LiveSubmissionController.java
@@ -2,11 +2,19 @@ package club.bayview.smoothieweb.controllers;
 
 import club.bayview.smoothieweb.SmoothieWebApplication;
 import club.bayview.smoothieweb.models.Submission;
+import club.bayview.smoothieweb.models.User;
+import club.bayview.smoothieweb.services.SmoothieProblemService;
 import club.bayview.smoothieweb.services.SmoothieSubmissionService;
+import club.bayview.smoothieweb.services.SmoothieUserService;
 import club.bayview.smoothieweb.services.WebSocketSessionService;
+import club.bayview.smoothieweb.util.NoPermissionException;
 import club.bayview.smoothieweb.util.NotFoundException;
+import club.bayview.smoothieweb.util.SessionUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
@@ -23,22 +31,43 @@ public class LiveSubmissionController implements WebSocketHandler {
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         SmoothieSubmissionService submissionService = SmoothieWebApplication.context.getBean(SmoothieSubmissionService.class);
+        SmoothieProblemService problemService = SmoothieWebApplication.context.getBean(SmoothieProblemService.class);
         WebSocketSessionService sessionService = SmoothieWebApplication.context.getBean(WebSocketSessionService.class);
+        ReactiveSessionRepository<Session> sessionRepository = SmoothieWebApplication.context.getBean(ReactiveSessionRepository.class);
 
         StringBuilder submissionId = new StringBuilder();
+        UnicastProcessor<WebSocketMessage> inputStream = UnicastProcessor.create(); // multiplex all input into one publisher
 
-        UnicastProcessor<WebSocketMessage> inputStream = UnicastProcessor.create();
-
-        return session.send(inputStream)
+        return session.send(inputStream) // set unicastprocessor as publisher for input
                 .and(session.receive()
+                        .take(1) // only receive one websocket message
                         .doOnNext(m -> {
                             String payload = m.getPayloadAsText(); // warning: getPayloadAsText actually takes the data from the buffer
-                            sessionService.addSession("/live-submission/" + payload, session, inputStream);
                             submissionId.append(payload);
 
-                            submissionService.findSubmissionById(payload)
+                            sessionRepository.findById(SessionUtils.getSessionIdFromHeader(session.getHandshakeInfo().getHeaders()))
                                     .switchIfEmpty(Mono.error(new NotFoundException()))
-                                    .doOnNext(s -> {
+                                    .flatMap(s -> {
+                                        // get user session from websocket http headers
+                                        SecurityContextImpl securityContext = s.getAttribute("SPRING_SECURITY_CONTEXT");
+                                        if (securityContext.getAuthentication() == null || !securityContext.getAuthentication().isAuthenticated() || !(securityContext.getAuthentication().getPrincipal() instanceof User)) {
+                                            return Mono.error(new NoPermissionException());
+                                        }
+
+                                        return Mono.zip(Mono.just(securityContext.getAuthentication()), submissionService.findSubmissionById(submissionId.toString()));
+                                    })
+                                    .switchIfEmpty(Mono.error(new NotFoundException()))
+                                    .flatMap(t -> Mono.zip(Mono.just(t.getT1()), Mono.just(t.getT2()), problemService.findProblemById(t.getT2().getProblemId())))
+                                    .switchIfEmpty(Mono.error(new NotFoundException()))
+                                    .flatMap(t -> {
+                                        Submission s = t.getT2();
+
+                                        // check session permission for submission
+                                        if (!s.hasPermissionToView(t.getT1(), t.getT3()))
+                                            return Mono.just(new NoPermissionException());
+
+                                        sessionService.addSession("/live-submission/" + submissionId.toString(), session, inputStream);
+
                                         List<Submission.SubmissionBatchCase> l = new ArrayList<>();
                                         for (var nl : s.getBatchCases()) l.addAll(nl);
 
@@ -48,10 +77,12 @@ public class LiveSubmissionController implements WebSocketHandler {
                                         } catch (JsonProcessingException e) {
                                             e.printStackTrace();
                                         }
+                                        return Mono.empty();
                                     })
                                     .doOnError(e -> inputStream.onNext(session.textMessage("[]")))
                                     .subscribe();
                         })
+                        .doOnError(e -> inputStream.onNext(session.textMessage("[]")))
                         .doFinally(signalType -> sessionService.removeSession("/live-submission/" + submissionId, session.getId())))
                 .then();
     }
--- src/main/java/club/bayview/smoothieweb/util/SessionUtils.java
+++ src/main/java/club/bayview/smoothieweb/util/SessionUtils.java
@@ -1,6 +1,7 @@
 package club.bayview.smoothieweb.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,6 +24,15 @@ public class SessionUtils {
                 }
             }
         }
+    }
+
+    public static String getSessionIdFromHeader(HttpHeaders headers) {
+        for (String s : headers.get("cookie").get(0).split(";")) {
+            if (s.split("=")[0].trim().equals("SESSION")) {
+                return s.split("=")[1].trim();
+            }
+        }
+        return null;
     }
 
 }
