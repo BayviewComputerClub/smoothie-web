f1271152e2867a1799ef9adfb1cdb3c192675518
EspiDev <espidev@gmail.com>
2020-03-09 17:10:30 -0400

    Fix component

--- src/main/java/club/bayview/smoothieweb/services/SmoothieStartup.java
+++ src/main/java/club/bayview/smoothieweb/services/SmoothieStartup.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Component
 public class SmoothieStartup {
 
     @Autowired
