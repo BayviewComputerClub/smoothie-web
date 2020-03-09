6461db8d3703c904bc41e4e1ec2483975b1bac24
EspiDev <espidev@gmail.com>
2020-03-09 17:29:14 -0400

    Fix startup

--- src/main/java/club/bayview/smoothieweb/services/SmoothieStartup.java
+++ src/main/java/club/bayview/smoothieweb/services/SmoothieStartup.java
@@ -7,16 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
-public class SmoothieStartup {
+public class SmoothieStartup implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
     private SmoothieUserService userService;
-
     @Autowired
     private SmoothieSettingsService settingsService;
 
@@ -26,9 +25,8 @@ public class SmoothieStartup {
     private Logger logger = LoggerFactory.getLogger(SmoothieStartup.class);
 
     // Default mongo objects
-    @PostConstruct
-    public void init() {
-
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         try {
             // create default admin account
             if (userService.findByUsername("admin").block() == null) {
