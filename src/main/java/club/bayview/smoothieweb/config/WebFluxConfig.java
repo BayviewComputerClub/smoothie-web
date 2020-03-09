70d258e85dff36e40ed6278160d756cbb1507e36
EspiDev <espidev@gmail.com>
2020-03-09 18:03:06 -0400

    Fixes

--- src/main/java/club/bayview/smoothieweb/config/WebFluxConfig.java
+++ src/main/java/club/bayview/smoothieweb/config/WebFluxConfig.java
@@ -1,10 +1,14 @@
 package club.bayview.smoothieweb.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.codec.multipart.MultipartHttpMessageReader;
 import org.springframework.http.codec.multipart.SynchronossPartHttpMessageReader;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebFluxConfig implements WebFluxConfigurer {
@@ -21,4 +25,10 @@ public class WebFluxConfig implements WebFluxConfigurer {
         configurer.defaultCodecs().multipartReader(multipartReader);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
+    }
 }
