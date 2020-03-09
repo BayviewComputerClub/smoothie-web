b4f77b62c07cec6934ca1fffd6c704b8f11f389b
Seshan Ravikumar <seshan10@me.com>
2020-03-08 21:01:07 -0400

    changes

--- src/main/java/club/bayview/smoothieweb/api/controllers/APIMainController.java
+++ src/main/java/club/bayview/smoothieweb/api/controllers/APIMainController.java
@@ -1,18 +1,32 @@
 package club.bayview.smoothieweb.api.controllers;
 
 import club.bayview.smoothieweb.models.GeneralSettings;
+import club.bayview.smoothieweb.models.User;
 import club.bayview.smoothieweb.services.SmoothieSettingsService;
+import club.bayview.smoothieweb.services.SmoothieUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class APIMainController {
     @Autowired
     private SmoothieSettingsService settingsService;
 
-    @RequestMapping("/api/home")
+    @Autowired
+    private SmoothieUserService userService;
+
+    @RequestMapping("/api/v1/home")
     public GeneralSettings getHomeSettings() {
         return settingsService.getGeneralSettings();
     }
+
+    @RequestMapping("/api/v1/ranking")
+    public List<User> getUserRanking() {
+        return userService.findUsers().collectList().block();
+    }
+
+
 }
