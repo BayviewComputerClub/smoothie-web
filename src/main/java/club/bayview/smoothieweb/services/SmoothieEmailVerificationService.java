dafd03840fd23931ff18899bb17c8dc4505eb620
Raymond Li <hi@raymond.li>
2020-03-16 23:44:59 -0400

    Create SmoothieEmailVerificationService

---
+++ src/main/java/club/bayview/smoothieweb/services/SmoothieEmailVerificationService.java
@@ -0,0 +1,64 @@
+package club.bayview.smoothieweb.services;
+
+import club.bayview.smoothieweb.models.User;
+import com.auth0.jwt.*;
+import com.auth0.jwt.algorithms.*;
+import com.auth0.jwt.exceptions.*;
+import com.auth0.jwt.interfaces.*;
+import com.auth0.jwt.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service
+public class SmoothieEmailVerificationService {
+
+    @Value("${smoothieweb.emailVerification.secret}")
+    String secret;
+
+    @Value("${smoothieweb.siteName}")
+    String siteName;
+
+    /**
+     * Create a new JWT for a user to send via email to verify
+     *
+     * @param user The user that needs to be verified
+     * @return The created JWT. Null on error.
+     */
+    public String createJWT(User user) {
+        try {
+            Algorithm alg = Algorithm.HMAC512(secret);
+            Calendar exp = Calendar.getInstance();
+            exp.add(Calendar.DATE, 1);
+            return JWT.create()
+                    .withIssuer(siteName)
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(exp.getTime())
+                    .withIssuedAt(Calendar.getInstance().getTime())
+                    .sign(alg);
+        } catch (JWTCreationException exception) {
+            return null;
+        }
+    }
+
+    /**
+     * Verify a JWT
+     *
+     * @param token The JWT to verify
+     * @return The username of the token, if verification was successful. Null otherwise.
+     */
+    public String verifyJWT(String token) {
+        try {
+            Algorithm alg = Algorithm.HMAC512(secret);
+            JWTVerifier verifier = JWT.require(alg)
+                    .withIssuer(siteName)
+                    .acceptLeeway(1)
+                    .build();
+            DecodedJWT jwt = verifier.verify(token);
+            return jwt.getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+}
